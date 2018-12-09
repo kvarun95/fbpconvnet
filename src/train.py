110ef2a087a8546c8cfdd0cd0fd7c593b4448081
kvarun95 <varunkelkar7795@gmail.com>
2018-12-09 16:04:56 -0600

    Custon training and test set sizes

--- src/train.py
+++ src/train.py
@@ -4,32 +4,34 @@ from generate_ellipse_dataset import *
 import tensorflow.keras.backend as K 
 
 
-def load_dataset(which='ellipses', forward='radon'):
+def load_dataset(train_size, test_size, which='ellipses', forward='radon'):
 
 	""" Defined in /src/train.py
 
 	Loads the dataset to be used for training and testing.
 	
 	Inputs : 
-	``which`` : String. specifies the type of dataset to be generated:
-		``'ellipses'``   : Ellipses dataset
-		``'biomedical'`` : Biomedical dataset
+	`train_size` : Size of training set
+	`test_size`  : size of test set
+	`which` : String. specifies the type of dataset to be generated:
+		`'ellipses'`   : Ellipses dataset
+		`'biomedical'` : Biomedical dataset
 
-	``forward`` : String. The forward model to be used 
-		``'radon'``		: Radon transform. inbuilt in the object ``ellipses``.
+	`forward` : String. The forward model to be used 
+		`'radon'`		: Radon transform. inbuilt in the object ``ellipses``.
 	"""
 
 	if which=='ellipses':
-		E = ellipses(25, (64,64))
-		E.create();
-		E.request_measurement(theta=np.arange(180), SNR=30);
+		E = ellipses(train_size+test_size, (128,128))
+		E.create(normalize='True');
+		E.request_measurement(theta=np.linspace(0., 179., 20), SNR=np.inf);
 		E.fbp_reconstruction();
 
-		X_train = E.recon[0:20].reshape(*(E.recon[0:20].shape), 1)
-		X_test = E.recon[20::].reshape(*(E.recon[20::].shape), 1)
+		X_train = E.recon[0:train_size].reshape(*(E.recon[0:train_size].shape), 1)
+		X_test = E.recon[train_size::].reshape(*(E.recon[train_size::].shape), 1)
 		
-		E_train = E.value[0:20].reshape(*(E.value[0:20].shape), 1)
-		E_test = E.value[20::].reshape(*(E.value[20::].shape), 1)
+		E_train = E.value[0:train_size].reshape(*(E.value[0:train_size].shape), 1)
+		E_test = E.value[train_size::].reshape(*(E.value[train_size::].shape), 1)
 
 	return X_train, X_test, E_train, E_test
 
